e0d0e52b1fc27f03bcef9280152fb74152dba98b
Benjamin Koehler <bkoehler751@gmail.com>
2020-09-16 23:15:10 -0700

    updated import from pycharm

--- queue/queue.py
+++ queue/queue.py
@@ -14,7 +14,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
 """
 
 
-from singly_linked_list import LinkedList
+from singly_linked_list.singly_linked_list import LinkedList
 
 class Queue:
     def __init__(self):
